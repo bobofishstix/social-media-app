a17df2c3b04f05e35cf04348c3525f2673ded920
bobofishstix <tynerbrad0313@gmail.com>
2021-05-31 15:30:04 -0500

    FIxed package.json Added Logo

--- social-media-app/src/containers/navbar/index.js
+++ social-media-app/src/containers/navbar/index.js
@@ -7,8 +7,10 @@ export default function Navbar() {
     const [user, setUser] = useContext(UserContext).user;
     return ( 
         <div className='navbar'>
-            <p>ReactSocial</p>
-
+            <div className='navbar-left'>
+                <img className='navbar-logo' src='https://th.bing.com/th/id/OIP.Q5inIO_EbwSo2Pj5vHFIDAHaDw?pid=ImgDet&w=840&h=426&rs=1' alt='BreadBook Logo'></img>
+                <p className='navbar-text'>BreadBook</p>
+            </div>
             {user ? <img className='navbar-img' src={user.photoURL}></img> : <SignInBtn />}
         </div>
     )
